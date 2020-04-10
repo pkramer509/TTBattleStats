f42e7358a53a3f26ffccf313551767705b9156ad
Patrick Kramer <patrick.kramer@gmail.com>
2020-04-09 19:26:58 -0500

    Changed database to xlsx file, added boolean filter contexts to ID major and player characters.

--- character.py
+++ character.py
@@ -4,12 +4,13 @@ class Character:
     def __init__(self, name, df):
         self.name = name
         self.df = df
+        self.damage_total = 0
+        self.damage_mean = 0
+        self.damage_median = 0
+        self.damage_mode = 0
 
-    def print_name(self):
-        print(self.name)
-
-    def damage_total(self):
-        return self.df.Damage.sum()
-
-    def damage_mean(self):
-        return self.df.Damage.mean()
+    def calculate_statistics(self):
+        self.damage_total = self.df.Damage.sum()
+        self.damage_mean = self.df.Damage.mean()
+        self.damage_median = self.df.Damage.median()
+        self.damage_mode = self.df.Damage.mode().max()
--- process.py
+++ process.py
@@ -1,38 +1,43 @@
 import pandas as pd
 import numpy as np
-import filters
 from character import Character
 
 pd.set_option("display.max_rows", 20, "display.max_columns", 12, 'display.width', 150)
 
-header = ["Round", "Order", "Character", "Event", "Attack", "Target", "AC", "DC", "Damage", "Assist"]
+header = ["Round", "Order", "Character", "Event", "Attack", "Target", "isMajor", "isPC", "AC", "DC", "Damage", "Assist"]
 datatype = {"Round":        "int",          # Combat Round
             "Order":        "int",          # Index within a round, used to handle multiple actions from same character
             "Character":    "string",       # Character Identifier String
             "Event":        "string",       # Event Identifier String (Attack|Heal|Death|Movement)
             "Attack":       "string",       # Attack Descriptive String (type accumulation)
-            "Target":       "string",       # Character Identifier String (Can be a list)
+            "Target":       "string",       # Character Identifier String
+            "isMajor":      "bool",         # Defines character is important, in contrast to grunts and meat popsicles
+            "isPC":         "bool",         # Defines character is a Player Character
             "AC":           "int",          # Adversarial AC roll
             "DC":           "int",          # Sympathetic DC Roll [leverage Attack Reference for threshold]
             "Damage":       "int",          # Damage Total (negative values represent healing)
             "Assist":       "string"}       # Character Identifier String (Can be a list)
 
-data = pd.read_csv("data/Jan-23-2020.csv",
-                   sep=",",
-                   names=header,
-                   encoding="utf-8")
+xl = pd.ExcelFile("data/data.xlsx")
 
-# Drop the header row
-data = data.drop(data.index[0])
+data = pd.DataFrame()
 
-# Set Data Types
+for sheet in xl.sheet_names:
+    data = xl.parse(sheet)
+
+# Set/Coerce Data Types
 for column in header:
+    print(column)
+
     if datatype[column] == "int":
         data[column] = pd.to_numeric(data[column], errors='coerce')
         data[column] = data[column].dropna().astype('int')
 
     if datatype[column] == "string":
         data[column] = data[column].dropna().astype('U').str.strip()
+
+    if datatype[column] == "bool":
+        data[column] = np.where(data[column] == 1, True, False)
 
 print(data.dtypes)
 
@@ -41,13 +46,16 @@ characters = dict()
 for ch in data['Character'].unique():
     characters[ch] = Character(ch, data.loc[data['Character'] == ch])
 
-
-
-
 for ch in characters:
-    characters[ch].print_name()
-    print(characters[ch].damage_total())
-    print(characters[ch].damage_mean())
+    characters[ch].calculate_statistics()
+    print(characters[ch].df)
+
+    # print(characters[ch].name)
+    # print(characters[ch].damage_total)
+    # print(characters[ch].damage_mean)
+    # print(characters[ch].damage_median)
+    # print(characters[ch].damage_mode)
     print("~~~")
 
-print(characters['Garrin'].df)
+#
+# print(characters['Garrin'].df)
